08cff5025860b2cf1545b55e3f84ffb06b23c915
Artur C. Sponchiado <artursponchi@gmail.com>
2020-10-09 20:14:22 -0300

    :sparkles: add cannonshot sound effect

---
+++ Assets/SoundEffects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip clip;
+    private float volume = 0.5f;
+
+    void Start() {
+        CannonSpawner.OnShoot += OnShoot;
+    }
+
+    private void OnShoot() {
+        Debug.Log("tiro!");
+        audioSource.PlayOneShot(clip, volume);
+    }
+}
